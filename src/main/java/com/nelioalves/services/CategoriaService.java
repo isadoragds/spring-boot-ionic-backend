017ed29f975c34ed3f8432c986b21e736a1b4a49
Isadora Gonçalves <isadorag92@yahoo.com.br>
2023-02-23 14:45:56 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/nelioalves/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.nelioalves.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.domain.Categoria;
+import com.nelioalves.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	//para fazer a injeção de dependencia
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
