8e63583de553cfbd6bd2c119b06ce8664aa420f5
Isadora Gonçalves <isadorag92@yahoo.com.br>
2023-03-01 15:23:39 -0300

    Busca de produtos por nomes e categorias

--- src/main/java/com/nelioalves/repositories/ProdutoRepository.java
+++ src/main/java/com/nelioalves/repositories/ProdutoRepository.java
@@ -1,10 +1,22 @@
 package com.nelioalves.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.nelioalves.domain.Categoria;
 import com.nelioalves.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+	//com o padrao de nome do metodo nao precisa da query abaixo, só se nao seguisse o padrao
+	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	@Transactional(readOnly = true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 }
---
+++ src/main/java/com/nelioalves/resources/utils/URL.java
@@ -0,0 +1,31 @@
+package com.nelioalves.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class URL {
+	
+	//metodo para decodificar um paramentro. ex: espaco vazio no nome
+		public static String decodeParam(String s) {
+			try {
+				return URLDecoder.decode(s, "UTF-8");
+			} 
+			catch (UnsupportedEncodingException e) {
+				return "";
+			}
+		}
+	
+	//static pq vai chamar o metodo sem instanciar a classe
+	public static List<Integer> decodeIntList(String s) {
+		String[] vet = s.split(",");
+		List<Integer> list = new ArrayList<>();
+		for (int i=0; i<vet.length; i++) {
+			list.add(Integer.parseInt(vet[i]));
+		}
+		return list;
+		//return Arrays.asList(s.split(",")).stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+	}
+ 
+}
