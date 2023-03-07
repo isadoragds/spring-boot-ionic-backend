7656de4840a8be9adfe8963948518a908bcff5d7
Isadora Gonçalves <isadorag92@yahoo.com.br>
2023-03-07 16:38:15 -0300

    Esqueci a senha

--- src/main/java/com/nelioalves/resources/AuthResource.java
+++ src/main/java/com/nelioalves/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.nelioalves.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nelioalves.dto.EmailDto;
 import com.nelioalves.security.JWTUtil;
 import com.nelioalves.security.UserSS;
+import com.nelioalves.services.AuthService;
 import com.nelioalves.services.UserService;
 
 @RestController
@@ -18,6 +22,8 @@ public class AuthResource {
 	
 	@Autowired
 	private JWTUtil jwtUtil;
+	@Autowired
+	private AuthService service;
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
@@ -27,4 +33,10 @@ public class AuthResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDto objDto) {
+		service.sendNewPassword(objDto.getEmail());
+		return ResponseEntity.noContent().build();
+	}
+	
 }
