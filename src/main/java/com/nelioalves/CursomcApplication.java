f57f904fcbb40edc10bc5d4e08cdea6c6e470d25
Isadora Gonçalves <isadorag92@yahoo.com.br>
2023-03-08 16:37:10 -0300

    Salvando primeiro arquivo no S3 (erro ao enviar - Unable to calculate MD5 hash)

--- src/main/java/com/nelioalves/CursomcApplication.java
+++ src/main/java/com/nelioalves/CursomcApplication.java
@@ -1,12 +1,17 @@
 package com.nelioalves;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nelioalves.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -14,5 +19,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 
 	public void run(String... args) throws Exception {
+		
+		s3Service.uploadFile("C:\\Users\\Nova\\Pictures\\hayleywilliams2023");
 	}
 }
