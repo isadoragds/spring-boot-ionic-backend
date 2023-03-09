d1d597b31fefba88c917d8cb54f436e5fb0044da
Isadora Gonçalves <isadorag92@yahoo.com.br>
2023-03-09 16:03:02 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/nelioalves/domain/Cliente.java
+++ src/main/java/com/nelioalves/domain/Cliente.java
@@ -60,7 +60,6 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageUrl;
 	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
@@ -174,13 +173,6 @@ public class Cliente implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
 
 
 	
--- src/main/java/com/nelioalves/services/ClienteService.java
+++ src/main/java/com/nelioalves/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.nelioalves.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -29,8 +31,8 @@ import com.nelioalves.services.exceptions.DataIntegrityException;
 
 @Service
 public class ClienteService {
-	
-	//para fazer a injeção de dependencia
+
+	// para fazer a injeção de dependencia
 	@Autowired
 	private ClienteRepository repo;
 	@Autowired
@@ -39,20 +41,26 @@ public class ClienteService {
 	private BCryptPasswordEncoder pe;
 	@Autowired
 	private S3Service s3Service;
-	
-	//recebe um id e retorna o cliente correspondente. se nao encontrar lanca excecao
+	@Autowired
+	private ImageService imageService;
+
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
+
+	// recebe um id e retorna o cliente correspondente. se nao encontrar lanca
+	// excecao
 	public Cliente find(Integer id) {
-		
+
 		UserSS user = UserService.authenticated();
 		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
 			throw new AuthorizationException("Acesso negado");
 		}
-		
+
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new com.nelioalves.services.exceptions.ObjectNotFoundException(
-				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
-	
+
 	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
@@ -60,71 +68,68 @@ public class ClienteService {
 		enderecoRepository.saveAll(obj.getEnderecos());
 		return obj;
 	}
-	
+
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
 		return repo.save(newObj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
-		repo.deleteById(id);
-		} 
-		catch(DataIntegrityViolationException e) {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
-	
+
 	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public Page<Cliente> findPage(int page, int linesPerPage, Direction direction, String orderBy) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, direction, orderBy);
 		return repo.findAll(pageRequest);
-	} 
-	
+	}
+
 	public Cliente fromDto(ClienteDto objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);
 	}
-	
+
 	public Cliente fromDto(ClienteNewDto objDto) {
-		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
 		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
-		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
 		cli.getEnderecos().add(end);
 		cli.getTelefones().add(objDto.getTelefone1());
 		if (objDto.getTelefone2() != null) {
 			cli.getTelefones().add(objDto.getTelefone2());
-		}if (objDto.getTelefone2() != null) {
+		}
+		if (objDto.getTelefone2() != null) {
 			cli.getTelefones().add(objDto.getTelefone3());
 		}
 		return cli;
 	}
-	
-	//metodo auxiliar dentro da classe, por isso é private
+
+	// metodo auxiliar dentro da classe, por isso é private
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
-	
-	//metodo para fazer upload de foto de perfil de cliente
+
+	// metodo para fazer upload de foto de perfil de cliente
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		
+
 		UserSS user = UserService.authenticated();
 		if (user == null) {
 			throw new AuthorizationException("Acesso negado");
 		}
-		
-		URI uri =  s3Service.uploadFile(multipartFile);
-		
-		Optional<Cliente> cli = repo.findById(user.getId());
-		cli.orElse(null).setImageUrl(uri.toString());
-		repo.save(cli.orElse(null));
-		
-		return uri;
+
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
-	
 }
